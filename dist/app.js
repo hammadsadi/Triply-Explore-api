bc1601fadd8387d5df1907afd1a86e305fea6e26
hammadsadi <hammad.sadi@yahoo.com>
2024-12-16 23:42:51 +0600

    Added Global API Route Error Handler

--- dist/app.js
+++ dist/app.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const notFound_1 = __importDefault(require("./app/middlewares/notFound"));
 const app = (0, express_1.default)();
 const port = 3000;
 // Parser
@@ -12,4 +13,6 @@ app.use(express_1.default.text());
 app.get("/", (req, res) => {
     res.send("Hello World..!");
 });
+// Global Middleware
+app.use(notFound_1.default);
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
-import express, { Request, Response } from "express";
-const app = express();
+import express, { Application, Request, Response } from "express";
+import notFound from "./app/middlewares/notFound";
+const app: Application = express();
 const port = 3000;
 
 // Parser
-app.use(express.json())
-app.use(express.text())
-app.get("/", (req:Request, res:Response) => {
+app.use(express.json());
+app.use(express.text());
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World..!");
 });
 
+// Global Middleware
+app.use(notFound);
 
-export default app
+export default app;
